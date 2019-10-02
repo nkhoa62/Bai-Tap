9d7fbb4204f55b657befb9b97430eb586c785bb9
DECADE <DECADE@DESKTOP-7NTUI8H>
2019-10-02 10:27:08 +0700

    AppOOp

---
+++ appOOP/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace appOOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string ten;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ok_Click(object sender, RoutedEventArgs e)
+        {
+            List<Student> student = new List<Student>();
+            student.Add(new Student ("Kuuga"));
+            student.Add(new Student ("Agito"));
+            student.Add(new Student ("Faizz"));
+
+            foreach (Student item in student)
+            {
+               item.Talk();
+               txbTen.Items.Add(item.StudentName);
+            }
+
+            for (int i = 1; i <= student.Count; i++)
+                txbSTT.Items.Add(i.ToString());
+        }
+
+    }
+}
---
+++ appOOP/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace appOOP
+{
+    public class Student
+    {
+        private string Name;
+        public string ten;
+
+        public string StudentName
+        {    
+            get { return Name; }   
+            set { Name = value; }
+        }
+        public Student (string Name)
+        {
+            this.Name = Name;
+        }
+        public virtual void Talk()
+        {
+            ten = Name;
+        }
+        public Student(){}
+    }
+}
